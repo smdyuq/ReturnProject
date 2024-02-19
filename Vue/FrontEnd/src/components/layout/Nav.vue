f3169c7ca6de501ecaaec072d942a899a9807f31
Jaeyun <gejon30@naver.com>
2024-02-19 15:44:33 +0900

    뷰 연동중

---
+++ Vue/FrontEnd/src/components/layout/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav style="display:flex; margin-bottom:20px; font-size:large;" >
+        <router-link to="/SalesManagement"><span style="color:black;">상품등록</span></router-link>
+        <router-link to="/ProductManagement"><span style="color:black;">상품관리</span></router-link>
+    </nav>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
