af58dc7c042072c12406572aa71e51a2ed723314
LeeSeungYeop <eric0966@naver.com>
2024-02-19 14:55:35 +0900

    2/19 관리자 페이지쪽 주석처리

--- ReturnProject/src/main/java/kr/co/three/admin/controller/adminPage.java
+++ ReturnProject/src/main/java/kr/co/three/admin/controller/adminPage.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class adminPage {
 
-//	관리자 페이지로 이동
-	@RequestMapping("/admin")
-	public String adminPage() {
-		return "admin/member/adminLogin";
-	}
+////	관리자 페이지로 이동
+//	@RequestMapping("/admin")
+//	public String adminPage() {
+//		return "admin/member/adminLogin";
+//	}
 }
